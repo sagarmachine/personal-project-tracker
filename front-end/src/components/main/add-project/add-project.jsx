34234e59b12ad5f9636453da645aa8b94963647e
anuj bhatt <anujbhatt023@gmail.com>
2020-06-27 16:56:08 +0530

    style add projet route

--- front-end/src/components/main/add-project/add-project.jsx
+++ front-end/src/components/main/add-project/add-project.jsx
@@ -7,38 +7,30 @@
 
      return (
         <div className="addProject">
-        <div class="project">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 m-auto">
-                        <h5 class="display-4 text-center">Create / Edit Project form</h5>
-                        <hr />
-                        <form>
-                            <div class="form-group">
-                                <input type="text" class="form-control form-control-lg " placeholder="Project Name" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control form-control-lg" placeholder="Unique Project ID"
-                                    disabled />
-                            </div>
-                           <div class="form-group">
-                                <textarea class="form-control form-control-lg" placeholder="Project Description"></textarea>
-                            </div>
-                            <h6>Start Date</h6>
-                            <div class="form-group">
-                                <input type="date" class="form-control form-control-lg" name="start_date" />
-                            </div>
-                            <h6>Estimated End Date</h6>
-                            <div class="form-group">
-                                <input type="date" class="form-control form-control-lg" name="end_date" />
-                            </div>
-
-                            <input type="submit" class="btn btn-primary btn-block mt-4" />
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+          <h5 className="addProject__heading">Create / Edit Project form</h5>
+<hr />
+          <div className="addProject__item">
+                <input type="text" className="addProject__item-name" placeholder="Project Name" />
+          </div>
+          <div className="addProject__item">
+                <input type="text" className="addProject__item-id" placeholder="Unique Project ID"/>
+          </div>
+          <div className="addProject__item">
+                <textarea className="addProject__item-des" placeholder="Project Description"></textarea>
+          </div>
+          <h6>Start Date</h6>
+          <div className="addProject__item">
+                <input type="date" className="addProject__item-date" name="start_date" />
+          </div>
+          <h6>Estimated End Date</h6>
+          <div className="addProject__item">
+                <input type="date" className="addProject__item-date" name="end_date" />
+          </div>
+          <div className="addProject__item">
+                <input type="submit" className="submitBtn" />
+          </div>
+          <div className="addProject__img">
+          </div>
         </div>
      )
    }
