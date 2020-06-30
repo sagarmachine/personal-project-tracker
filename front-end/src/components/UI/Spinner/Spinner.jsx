efcc2ccb6d84697efecb50400bcbb392890bb014
anuj bhatt <anujbhatt023@gmail.com>
2020-06-30 12:24:05 +0530

    added spinner and fix delete

---
+++ front-end/src/components/UI/Spinner/Spinner.jsx
@@ -0,0 +1,15 @@
+ import React, {Component} from "react"
+
+
+ class Spinner extends Component{
+
+   render(){
+
+     return (
+        <div><div class="loader">Loading...</div></div>
+     )
+   }
+ }
+
+
+export default Spinner;
--- front-end/src/components/main/dashboard/project-items/project-item/project-item.jsx
+++ front-end/src/components/main/dashboard/project-items/project-item/project-item.jsx
@@ -1,16 +1,10 @@
  import React, {Component} from "react"
-import axios from "axios";
-import {Link} from "react-router-dom"
+// import axios from "axios";
+// import {Link} from "react-router-dom"
 
  class ProjectItem extends Component{
 
-   onClickHandler=()=>{
-     axios.delete("/v1/project/"+this.props.name).then(res=>{
-       console.log(res);
-     }).catch(e=>{
-       console.log(e);
-     })
-   }
+
 
 
    render(){
@@ -24,21 +18,15 @@ import {Link} from "react-router-dom"
                     </div>
                     <div className="projectItem__edits">
                         <ul className="projectItem__edits-list">
-                            <Link to="">
                                 <li className="projectItem__edits-board projectItem__edits-item">
                                     <i className="fa fa-flag-checkered pr-1"> Project Board</i>
                                 </li>
-                            </Link>
-                            <Link to="">
                                 <li className="projectItem__edits-update projectItem__edits-item">
                                     <i className="fa fa-edit pr-1"> Update Project Info</i>
                                 </li>
-                            </Link>
-                            <Link to="/dashboard">
-                                <li onClick={this.onClickHandler} className="projectItem__edits-delete projectItem__edits-item">
+                                <li onClick={()=>this.props.delete(this.props.identifier,this.props.index)} className="projectItem__edits-delete projectItem__edits-item">
                                     <i className="fa fa-minus-circle pr-1"> Delete Project</i>
                                 </li>
-                            </Link>
                         </ul>
                     </div>
                 </div>
