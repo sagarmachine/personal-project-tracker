658accf6f58c48fe029ba8e8d6bab11a52e82a26
anuj bhatt <anujbhatt023@gmail.com>
2020-07-09 14:54:54 +0530

    add project detail view

--- front-end/src/components/main/project-board/detail-view/project-detail-view/project-detail-view.jsx
+++ front-end/src/components/main/project-board/detail-view/project-detail-view/project-detail-view.jsx
@@ -8,8 +8,34 @@ export default class ProjectDetailView extends Component {
 
     render() {
         return (
-            <div className="ProjectDetailView">
-                <div></div>
+            <div className="taskDetailView">
+                <div className="taskDetailView__btn taskDetailView__btn--modifier">
+                    <div className="ProjectDetailView__name"><strong>PROJECT NAME: </strong>{this.props.projectData.projectName}</div>
+                    <div className="ProjectDetailView__identifier"><strong>PROJECT IDENTIFIER: </strong>{this.props.projectData.projectIdentifier}</div>
+                </div>
+                <div className="taskDetailView__btn taskDetailView__btn--modifier">
+                    <div className="ProjectDetailView__name"><strong>START DATE: </strong>{this.props.projectData.startingDate}</div>
+                    <div className="ProjectDetailView__identifier"><strong>END DATE: </strong>{this.props.projectData.endingDate}</div>
+                </div>
+                <h4>DESCRIPTION</h4>
+                <div className="ProjectDetailView__des">{this.props.projectData.projectDescription}</div>
+                <h4>NOTES</h4>
+                <div className="taskDetailView__notes">
+                   {this.props.projectData.notes.map((note,i)=>(
+                     <div key={"note"+i} className="taskDetailView__note">
+                        {note}
+                     </div>
+                   ))}
+                </div>
+                <h4>LINKS</h4>
+                <div className="taskDetailView__links">
+                  {this.props.projectData.usefullLinks.map((link,i)=>(
+                    <div key={"link"+i} className="taskDetailView__link">
+                       <div className="taskDetailView__linkUR">{link.link}</div>
+                       <div className="taskDetailView__comment">{link.comment}</div>
+                    </div>
+                  ))}
+                </div>
             </div>
         )
     }
