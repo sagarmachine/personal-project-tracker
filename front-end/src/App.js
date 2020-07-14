5d7590ae4e4ec23fc986d06c1a5a2d3461c2104e
sagarmachine <sagarmonkey800@gmail.com>
2020-07-14 09:21:14 +0530

    aws ready

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,8 +4,8 @@ import Layout from "./components/layout/layout"
 import axios from "axios"
 
 axios.defaults.baseURL=
-                        // "http://localhost:8081"
-                       "https://projecttracker-env.eba-g6te9sw4.ap-south-1.elasticbeanstalk.com"
+                         "http://localhost:8081"
+                       //"https://projecttracker-env.eba-g6te9sw4.ap-south-1.elasticbeanstalk.com"
                               +"/api";
 
 function App() {
